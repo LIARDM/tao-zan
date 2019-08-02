4bac410ca0b08d5e1f7e4602fc6a920b99829ac2
Furie_Celeste <mevenliard@gmail.com>
2019-08-02 18:15:10 +0200

    Build 0.7 : Mise en forme améliorée.
    Amélioration du rendu visuel des régions validées.

--- angular-mephala-tirage/src/app/app.module.ts
+++ angular-mephala-tirage/src/app/app.module.ts
@@ -3,13 +3,16 @@ import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+
 import { MatButtonModule } from '@angular/material/button';
 import { MatRadioModule} from '@angular/material';
 import { MatSelectModule} from '@angular/material/select';
 import { MatIconModule } from '@angular/material/icon'
 import { MatInputModule} from '@angular/material/input';
-import {MatDividerModule} from '@angular/material/divider';
-import {MatListModule} from '@angular/material/list';
+import { MatDividerModule} from '@angular/material/divider';
+import { MatListModule} from '@angular/material/list';
+import { MatTreeModule} from '@angular/material/tree';
+
 
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
@@ -37,6 +40,7 @@ import { from } from 'rxjs';
     MatIconModule,
     MatDividerModule,
     MatListModule,
+    MatTreeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
